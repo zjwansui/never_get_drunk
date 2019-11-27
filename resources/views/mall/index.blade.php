ecc022828bc9e6976f79d99aac8ad76509e2d5e0
zhangjie <629041893@qq.com>
2019-11-27 20:02:13 +0800

    z

---
+++ resources/views/mall/index.blade.php
@@ -0,0 +1,69 @@
+@extends('default')
+
+@section('contents')
+
+
+
+    {{--<div class="row">--}}
+    {{--<div class="col-xs-2" style="background: #ff743c;height: 20px">--}}
+    {{--</div>--}}
+    {{--<div class="col-xs-2" style="background: #68ff4a;height: 20px">--}}
+    {{--</div>--}}
+    {{--<div class="col-xs-2" style="background: #fffb0d;height: 20px">--}}
+    {{--</div>--}}
+    {{--<div class="col-xs-2" style="background: #6bb1ff;height: 20px">--}}
+    {{--</div>--}}
+    {{--<div class="col-xs-2" style="background: #5dff9c;height: 20px">--}}
+    {{--</div>--}}
+    {{--<div class="col-xs-2" style="background: #ffb5bd;height: 20px">--}}
+    {{--</div>--}}
+    {{--</div>--}}
+
+    <div class="col-xs-12">
+        <p class="lead"> <img src="../img/sign.jpg" alt="商标" class="img-thumbnail img-responsive" style="max-width:20%">
+        厘革·手作</p>
+
+<p class="lead">简介：</p>
+        <p><img src="../img/tell.jpg" alt="简介" class="img-thumbnail img-responsive"></p>
+
+        <p>定义：</p>
+
+        <p><img src="../img/qq.jpg" alt="定义" class="img-thumbnail img-responsive"></p>
+
+    <p class="lead">工作：</p>
+
+
+
+    <table class="table">
+       <tr>
+           <td>
+               <img src="./../img/22.jpg" alt="公司图片" class="img-rounded img-responsive" >
+           </td>
+           <td>
+               <img src="./../img/33.jpg" alt="公司图片" class="img-rounded img-responsive">
+           </td>
+       </tr>
+
+        <tr>
+            <td>
+                <img src="./../img/11.jpg" alt="公司图片" class="img-rounded img-responsive" >
+            </td>
+            <td>
+                <img src="./../img/44.jpg" alt="公司图片" class="img-rounded img-responsive">
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <img src="./../img/55.jpg" alt="公司图片" class="img-rounded img-responsive" >
+            </td>
+            <td>
+                <img src="./../img/66.jpg" alt="公司图片" class="img-rounded img-responsive">
+            </td>
+        </tr>
+    </table>
+
+    </div>
+
+
+@stop
